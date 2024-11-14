8728956c5da8e08e7f69ba12077a974301e36b52
Матвей Клёнов <mklenov@dev.vtb.ru>
2024-11-14 17:57:57 +0300

    Секция options на моблики

---
+++ src/pages/main/sections/options/types.ts
@@ -0,0 +1,9 @@
+import type { ImageMetadata } from 'astro'
+
+export interface OptionsCard {
+	title: string
+	description: string
+	detailedDescription: string
+	smImgSrc: ImageMetadata['src']
+	lgImgSrc?: ImageMetadata['src']
+}

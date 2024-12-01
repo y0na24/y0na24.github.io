f13babd3d9f9bb8861cd28037000116895a27a62
Матвей Клёнов <mklenov@dev.vtb.ru>
2024-12-01 16:14:54 +0300

    fix

--- src/pages/main/sections/options/types.ts
+++ src/pages/main/sections/options/types.ts
@@ -1,11 +1,10 @@
 import type { ImageMetadata } from 'astro'
 
 export interface OptionsCard {
-	
 	title: string
 	description: string
 	detailedDescription: string
-	smImgSrc: ImageMetadata['src']
-	lgImgSrc?: ImageMetadata['src']
-	mdImgSrc?: ImageMetadata['src']
+	smImgSrc: ImageMetadata
+	lgImgSrc: ImageMetadata
+	mdImgSrc: ImageMetadata
 }

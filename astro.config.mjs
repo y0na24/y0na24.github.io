6680207fdeba8ddfe4207e3714c6117bb5ab844d
Матвей Клёнов <mklenov@dev.vtb.ru>
2024-12-01 15:28:25 +0300

    add actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from 'astro/config'
 
+const HOST = 4321
+const LIVE_URL = 'https://y0na24.github.io'
+const LOCALHOST_URL = `http://localhost:${HOST}/`
+const SCRIPT = process.env.npm_lifecycle_script ?? ''
+const isBuild = SCRIPT.includes('astro build')
+let BASE_URL = LOCALHOST_URL
+
+if (isBuild) {
+	BASE_URL = LIVE_URL
+}
+
 // https://astro.build/config
 export default defineConfig({
 	redirects: {
 		'/': '/main',
 	},
-	// site: 'https://y0na24.github.io',
+	site: BASE_URL,
 })
